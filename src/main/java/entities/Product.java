c0c83d882588c2faffec10268de540a6f5388b37
dominikhustava <dominikhustava1@gmail.com>
2022-07-08 17:16:33 +0200

    first entity Product with @Entity, @Basic, @Column. Updated persistance-xml to get it work

---
+++ src/main/java/entities/Product.java
@@ -0,0 +1,52 @@
+package entities;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+public class Product {
+
+    @Id
+    private int id;
+
+    @Basic(optional = false)//must be filled
+    private String name;
+
+    //price should not be double but decimal
+    private double price;
+
+    @Column(name = "expiration_date")
+    private LocalDate expirationDate;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public LocalDate getExpirationDate() {
+        return expirationDate;
+    }
+
+    public void setExpirationDate(LocalDate expirationDate) {
+        this.expirationDate = expirationDate;
+    }
+}
---
+++ src/main/java/main/Main.java
@@ -0,0 +1,32 @@
+package main;
+
+import entities.Product;
+
+import javax.persistence.Persistence;
+import java.time.LocalDate;
+
+public class Main {
+    public static void main(String[] args) {
+        // EntityManager
+        // manages the context of entities (collection of instances(of entities))
+
+        var emf = Persistence.createEntityManagerFactory("my-persistence-unit");
+        var em = emf.createEntityManager();
+
+        Product product = new Product();
+        product.setId(100);
+        product.setName("Beer");
+        product.setPrice(10.5);
+        product.setExpirationDate(LocalDate.now());
+
+        try {
+            em.getTransaction().begin();
+
+            em.persist(product);//adding the instance in the context (it's not insert)
+
+            em.getTransaction().commit();
+        }catch (Exception e){
+            em.getTransaction().rollback();
+        }
+    }
+}
