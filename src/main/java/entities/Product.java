72e38533037c83078b6fe342f347b13c21bc6af6
dominikhustava <dominikhustava1@gmail.com>
2022-07-13 12:03:38 +0200

    @TableGenerator for customizations in GenerationType.TABLE underlying table in DB

--- src/main/java/entities/Product.java
+++ src/main/java/entities/Product.java
@@ -7,13 +7,20 @@ import java.time.LocalDate;
 public class Product {
 
     @Id
+    @TableGenerator(name = "key_generator",
+            pkColumnName = "key_name", pkColumnValue = "Product",
+            valueColumnName = "key_value"
+            //allocationSize = 20 //for id intervals
+
+    )
     @GeneratedValue(strategy = GenerationType.TABLE, generator = "key_generator")
     private Integer id;
 /*TABLE type is similar to sequence(sequence not supported mysql)
 * We have to create table in DB with custom name -> in this example "key_generator"
-* It has to have 2 columns with default names:
-* sequence_name(VARCHAR(100))
-* next_val(INT)
+* It has to have 2 columns with  names:
+* key_name(VARCHAR(100))
+* key_value(INT)
+* for this entity the value in key_name(pkColumnValue) will be "Product"
 * */
 
 
