cd6fd6ed4c005162fb10f34aea8ad88efd272a87
dominikhustava <dominikhustava1@gmail.com>
2022-07-18 13:02:16 +0200

    ManyToOne relationship using foreign key with defaul column name

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -19,17 +19,26 @@ public class Main {
         try {
             em.getTransaction().begin();
 
-        Employee e1 = new Employee();
-        e1.setName("EMP 1");
+//        Employee e1 = new Employee();
+//        e1.setName("EMP 1");
+//
+//        Department d1 = new Department();
+//        d1.setName("DEP 1");
+//        d1.setEmployees(new ArrayList<>());
+//        d1.getEmployees().add(e1);
+//
+//        em.persist(e1);
+//        em.persist(d1);
 
-        Department d1 = new Department();
-        d1.setName("DEP 1");
-        d1.setEmployees(new ArrayList<>());
-        d1.getEmployees().add(e1);
+            Person p1 = new Person();
+            p1.setName("P1");
 
-        em.persist(e1);
-        em.persist(d1);
+            Document d1 = new Document();
+            d1.setName("DOC 1");
 
+            d1.setPerson(p1);
+            em.persist(p1);
+            em.persist(d1);
 
             em.getTransaction().commit();
         }catch (Exception e){
